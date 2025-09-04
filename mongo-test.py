96ccd7136e86eb98ec1de685b4b1afc2a64c9afb
Patryk Mikuli <patryk.mikuli@gmail.com>
2025-09-04 14:39:06 +0200

    Remove anything related to mongo

--- mongo-test.py
+++
@@ -1,9 +0,0 @@
-import pymongo
-
-client = pymongo.MongoClient('mongodb://root:password@localhost:27017')
-db = client['database']
-collection = db['test']
-
-document = {"test": "test"}
-x = collection.insert_one(document)
-print(x)
